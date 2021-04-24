e90c611ad146bdf879ed78e1bd64e82899dfc5df
Bohdan <bogdan.lybinsky@gmail.com>
2021-04-24 13:25:53 +0300

    dds

--- ConsoleApp23/Program.cs
+++ ConsoleApp23/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace ConsoleApp23
         static void Main(string[] args)
         {
             Console.WriteLine("Main");
+            Class1 obj1 = new Class1();
+            obj1.MyProperty = 44;
         }
     }
 }
