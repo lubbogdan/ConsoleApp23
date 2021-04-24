b1909fa8ff1ea12e9f149006bf416dab46472d34
Bohdan <bogdan.lybinsky@gmail.com>
2021-04-24 13:30:08 +0300

    rtr

--- ConsoleApp23/Program.cs
+++ ConsoleApp23/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace ConsoleApp23
         static void Main(string[] args)
         {
             Console.WriteLine("Main");
+            Class2 obj2 = new Class2();
+            obj2.MyProperty2 = 55;
         }
     }
 }
